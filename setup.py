dee6c99c6283565ce0e15c43a6b4c79395cf7228
luozhouyang <zhouyang.luo@gmail.com>
2019-08-21 21:50:29 +0800

    Publish v0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="easylib",
-    version="0.0.5",
+    version="0.0.6",
     description="Frequently used functions and modules library for Python 3 by ZhouYang Luo.",
     long_description=long_description,
     long_description_content_type="text/markdown",
