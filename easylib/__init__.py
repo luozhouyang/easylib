b6b2d1aadae255d33fd290378360e7fd8d35ce3b
luozhouyang <zhouyang.luo@gmail.com>
2019-08-21 21:41:40 +0800

    Remove logger initialization

--- easylib/__init__.py
+++
@@ -1,6 +0,0 @@
-from easylib.log import TimedRotatingLoggerInitializer
-
-name = 'easylib'
-
-initializer = TimedRotatingLoggerInitializer(name='easylib', path='/tmp/easylib.log')
-initializer.initialize()
